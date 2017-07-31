4bfb4139e6538ba735bfe4722bd12163ea31f3f9
James Laird-Wah <james@laird-wah.net>
2017-07-31 16:13:51 +1000

    remove test code from test.c
    ...which needs renaming

--- test.c
+++ test.c
@@ -4,62 +4,6 @@
 #include <string.h>
 #include "disc_format/cdparse.h"
 
-void test_file_io(void) {
-    // hacky file testing.
-    char buf[512];
-    int i;
-    buf[0] = 'f';
-    buf[1] = 0;
-
-    int fd = s_open("foo.bar", FA_READ|FA_WRITE|FA_CREATE_ALWAYS);
-    for (i=0; i<128; i++)
-        buf[i] = 3*i;
-    s_write(fd, buf, 128);
-    s_close(fd);
-
-    fd = s_open("foo.bar", FA_READ|FA_WRITE);
-    s_seek(fd, 96, C_SEEK_SET);
-    s_truncate(fd);
-    s_seek(fd, 48, C_SEEK_SET);
-    s_read(fd, buf, 32);
-    for (i=0; i<32; i++)
-        buf[i] &= 0xf0;
-    s_seek(fd, 48, C_SEEK_SET);
-    s_write(fd, buf, 32);
-    s_close(fd);
-
-    s_rename("foo.bar", "baz.foo");
-    
-    i = s_stat("baz.foo", (void*)buf, sizeof(buf));
-    s_stat_t * stat = (void*)buf;
-    fd = s_open("out", FA_READ|FA_WRITE|FA_CREATE_ALWAYS);
-    s_write(fd, stat, i+9);
-    s_close(fd);
-
-    s_mkdir("blonk");
-
-    s_opendir(".");
-    fd = s_open("out2", FA_READ|FA_WRITE|FA_CREATE_ALWAYS);
-    while ((i = s_stat(NULL, stat, sizeof(buf))) > 0) {
-        s_write(fd, stat, i+9);
-    }
-    s_close(fd);
-
-    s_chdir("blonk");
-    s_rename("../out2", "bloop");
-
-    s_chdir("..");
-    s_unlink("baz.foo");
-}
-
-void test_stdio(void) {
-    FILE *fp = fopen("stdio.txt", "w");
-    fprintf(fp, "Hello world!\n");
-    fwrite("123456", 6, 1, fp);
-    fputc(0x55, fp);
-    fclose(fp);
-}
-
 #include "menu.h"
 static file_ent * list_files(const char *dir, int *entries) {
     *entries = 0;
