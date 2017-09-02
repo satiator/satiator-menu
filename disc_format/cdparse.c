b18877f461f3cb9aad624e949db47e5d193b7ad3
James Laird-Wah <james@laird-wah.net>
2017-09-03 00:45:48 +1000

    cdparse: handle ISO files

--- disc_format/cdparse.c
+++ disc_format/cdparse.c
@@ -5,9 +5,53 @@
  * file, you can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include <string.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <endian.h>
+#include "satisfier.h"
 #include "cdparse.h"
 
 const char *cdparse_error_string = NULL;
+
+int iso2desc(const char *infile, const char *outfile) {
+    FILE *out = fopen(outfile, "wb");
+    if (!out) {
+        cdparse_error_string = "Can't open output file";
+        return -1;
+    }
+
+    struct stat st;
+    int ret = stat(infile, &st);
+    if (ret < 0) {
+        cdparse_error_string = "Could not stat ISO file";
+        return -1;
+    }
+
+    uint16_t h_nseg = htole16(1);
+    fwrite(&h_nseg, 2, 1, out);
+
+    seg_desc_t desc;
+    memset(&desc, 0, sizeof(desc));
+
+    desc.track = 1;
+    desc.index = 1;
+    desc.q_mode = 0x41;
+    desc.start = htole32(150);
+    desc.length = htole32(st.st_size / 2048);
+    desc.secsize = htole16(2048);
+    desc.filename_offset = htole32(2 + sizeof(desc));
+    desc.file_offset = htole32(0);
+
+    fwrite(&desc, sizeof(desc), 1, out);
+
+    uint8_t filename_len = strlen(infile);
+    fwrite(&filename_len, 1, 1, out);
+    fwrite(infile, filename_len, 1, out);
+
+    fclose(out);
+    return 0;
+}
 
 int image2desc(const char *infile, const char *outfile) {
    cdparse_error_string = NULL;
@@ -22,6 +66,9 @@ int image2desc(const char *infile, const char *outfile) {
 
    if (!strcasecmp(extension, "cue"))
       return cue2desc(infile, outfile);
+
+   if (!strcasecmp(extension, "iso"))
+      return iso2desc(infile, outfile);
 
    cdparse_error_string = "Unrecognised file extension";
    return 1;
