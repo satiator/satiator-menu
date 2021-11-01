24eaa3f2cec5fea0546d1f94bc0671add2b2b9b7
retrohead <retro_head@hotmail.com>
2021-11-01 22:56:38 +0000

    pipeline and revert makefile

--- build-newlib.sh
+++ build-newlib.sh
@@ -5,7 +5,6 @@ pwd
 set -e
 
 PREFIX=`pwd`/prefix/
-PROGPREFIX=sh2-elf-
 
 mkdir -p ${PREFIX}
 
@@ -15,23 +14,8 @@ function disable_flags () {
     done
 }
 
-
-export TARGET=sh-elf
-export CC_FOR_TARGET=${PROGPREFIX}gcc
-export LD_FOR_TARGET=${PROGPREFIX}ld
-export AS_FOR_TARGET=${PROGPREFIX}as
-export AR_FOR_TARGET=${PROGPREFIX}ar
-export RANLIB_FOR_TARGET=${PROGPREFIX}ranlib
 export TARGET_CFLAGS="-m2 -fno-stack-protector"
-${NEWLIB_SRC}/configure --prefix=${PREFIX} \
-                        --program-prefix=${PROGPREFIX} \
-                        --target=${TARGET} \
-                        --enable-lite-exit \
-                        --enable-newlib-nano-formatted-io \
-                        --enable-newlib-nano-malloc \
-                        --enable-target-optspace \
-                        --enable-newlib-reent-small \
-                        --disable-multilib $(disable_flags)
+${NEWLIB_SRC}/configure --prefix=${PREFIX} --target=sh-none-elf $(disable_flags) --enable-lite-exit --enable-newlib-nano-formatted-io --enable-newlib-nano-malloc --enable-target-optspace --enable-newlib-reent-small --disable-multilib
 
 make -j8
 make install
